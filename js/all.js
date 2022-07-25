3b338a62ebf02475ab011ef8350545eaf35b7736
Rys3t_リセット <Rys3t@users.noreply.github.com>
2022-07-25 12:36:10 +0800

    update gameid list and jquery bug fixed

--- js/all.js
+++ js/all.js
@@ -2,10 +2,15 @@ $(document).ready(function () {
   $(".images img").click(function(){
     $("#full-image").attr("src", $(this).attr("src"));
     $('#image-viewer').show();
+    $("h2").hide();
+    $('#footer').hide();
+    
   });
   
   $("#image-viewer .close").click(function(){
     $('#image-viewer').hide();
+    $("h2").show();
+    $('#footer').show();
   });
 });
 
